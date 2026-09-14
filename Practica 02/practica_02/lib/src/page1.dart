04384511da95b9f4d04099db43cb2c80b2c4dbbc
Jona_Balles <JonatanBenjaminx7@gmail.com>
2026-09-13 22:27:02 -0700

    Practicas 3 y 4

---
+++ Practica 02/practica_02/lib/src/page1.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.tealAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text('Practica 02'),
+        centerTitle: true,
+      ), // AppBar
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Pagina 2'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber, foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Page2');
+              },
+            ), // ElevatedButton
+            ElevatedButton(
+              child: Text('Pagina 3'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red, foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Page3');
+              },
+            ), // ElevatedButton
+          ], // <Widget>[]
+        ), // Column
+      ), // Center
+    ); // Scaffold
+  }
+}
